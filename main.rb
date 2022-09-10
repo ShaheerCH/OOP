4baba65f244e5d1f1600a63a09d40688a3e05dad
shaheer ch <shaheer.chry@gmail.com>
2022-09-11 00:41:06 +0500

    linters fixed

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require './student'
 
 def main
-  student-x = Student.new(age: 10, name: 'Ak', classroom: '5th Grade')
-  puts "Can student named #{student-x.name} use service?"
-  puts "R/ #{student-x.can_use_service?}"
-  puts student-x.play_hooky
+  student_x = Student.new(age: 10, name: 'Ak', classroom: '5th Grade')
+  puts "Can student named #{student_x.name} use service?"
+  puts "R/ #{student_x.can_use_service?}"
+  puts student_x.play_hooky
 end
